d754c6b54939418571f32d7ac6e4df35b106268f
P03W <pweidman.mail@gmail.com>
2020-12-07 22:45:55 -0500

    Make dodge do a minimum velocity (with some limits)

--- src/main/kotlin/mc/comdos/Comdos.kt
+++ src/main/kotlin/mc/comdos/Comdos.kt
@@ -19,19 +19,9 @@ class Comdos : ModInitializer {
         ServerSidePacketRegistry.INSTANCE.register(ROLL_PACKET_ID) { packetContext: PacketContext, _: PacketByteBuf? ->
             val player = packetContext.player
             packetContext.taskQueue.execute {
-                if (player.isAlive && (player as RollCooldownTrackable).rollCooldown <= 0 && (player as SoftVelocityTrackable).softVelocity.length() > 0.137f) {
-                    var newVelocity = (player as SoftVelocityTrackable).softVelocity.multiply(7.0)
-
-                    if (!player.isOnGround) {
-                        newVelocity = newVelocity.multiply(0.2)
-                    }
-
-                    player.velocity = Vec3d(newVelocity.x, player.velocity.y + 0.1, newVelocity.z)
-                    (player as ServerPlayerEntity).networkHandler.sendPacket(EntityVelocityUpdateS2CPacket(player))
-                    (player as RollCooldownTrackable).rollCooldown = 25.0f
-
-                    (player as JoinInvulnerabilityTicksAccessor).setSpawnInvulnerabilityTicks(8)
-                }
+                PLAYERS_DODGING[player as ServerPlayerEntity] = 8
+                (player as RollCooldownTrackable).rollCooldown = 25.0f
+                (player as JoinInvulnerabilityTicksAccessor).setSpawnInvulnerabilityTicks(2)
             }
         }
 
@@ -53,11 +43,32 @@ class Comdos : ModInitializer {
                     (it as RollCooldownTrackable).rollCooldown -= 1
                 }
             }
+
+            for (entry in PLAYERS_DODGING) {
+                val player = entry.key
+                val speed = (player as SoftVelocityTrackable).softVelocity.length()
+                if (player.isAlive && speed > 0.05f) {
+                    if (speed < 0.5f) {
+                        val newVelocity = (player as SoftVelocityTrackable).softVelocity.normalize().multiply(0.5)
+
+                        player.velocity = Vec3d(newVelocity.x, player.velocity.y, newVelocity.z)
+                        (player as ServerPlayerEntity).networkHandler.sendPacket(EntityVelocityUpdateS2CPacket(player))
+                    }
+                }
+
+                entry.setValue(entry.value - 1)
+
+                if (entry.value <= 0) {
+                    PLAYERS_DODGING.remove(player)
+                }
+            }
         })
     }
 
     companion object {
         val SYNC_VELOCITY_PACKET_ID = Identifier("comdos", "velocity_sync")
         val ROLL_PACKET_ID = Identifier("comdos", "roll")
+
+        val PLAYERS_DODGING: MutableMap<ServerPlayerEntity, Int> = mutableMapOf()
     }
 }
